791a7c25747de140ee7a2e3c10b327a8fce65081
Nyxvectar Yan <Nyxvectar@proton.me>
2025-01-30 12:06:28 +0800

    added some simple codes to input.rs

--- src/input/input.rs
+++ src/input/input.rs
@@ -8,5 +8,9 @@ pub mod get_input {    fn read_input<T: std::str::FromStr>() -> Result<Vec<T>, B
 where
     <T as std::str::FromStr>::Err: Error + 'static,
 {
-
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    let result: Result<Vec<T>, _> =
+        input.trim().split_whitespace().map(|s| s.parse()).collect();
+    result.map_err(|e| e.into())
 }}
