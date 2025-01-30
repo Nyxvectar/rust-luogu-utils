6a631823f70aec470fa8714ee53e2ad4f03dde1d
Nyxvectar Yan <Nyxvectar@proton.me>
2025-01-30 11:48:51 +0800

    added input.rs

--- src/input/input.rs
+++ src/input/input.rs
@@ -4,4 +4,9 @@
  * Created: 01/30/2025
  */
 
-pub mod get_input {}
+pub mod get_input {    fn read_input<T: std::str::FromStr>() -> Result<Vec<T>, Box<dyn Error>>
+where
+    <T as std::str::FromStr>::Err: Error + 'static,
+{
+
+}}
