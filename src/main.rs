4c26a6caf026a9eae7dfcabc753c40d8bf3ec42e
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-30 13:28:28 +0800

    added todos maybe impossible to manage for a lazy femboy

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
  * Created: 01/30/2025
  */
 
+// TODO: complete several functions in the last few months of 2025.
+
 fn main() {
-    println!("Hello, world!");
+    println!("Nyxvectar Yan how lazy are you...");
 }
